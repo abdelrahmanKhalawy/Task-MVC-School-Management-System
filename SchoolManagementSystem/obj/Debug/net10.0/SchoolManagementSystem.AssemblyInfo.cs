f114c066b36f685a0a8c322468ffcf9c2798e4d0
abdelrahmanKhalawy <abdovectort@gmail.com>
2026-04-07 15:51:46 +0200

    ignore edits

--- SchoolManagementSystem/obj/Debug/net10.0/SchoolManagementSystem.AssemblyInfo.cs
+++ SchoolManagementSystem/obj/Debug/net10.0/SchoolManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5251db7a0376cdc6b129dc1f572d9788b61b863f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08194ca9a12a406f089b4559707169dbfcf3ac23")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
